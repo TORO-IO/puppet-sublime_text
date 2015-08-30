6c25be7297898fd19121b1e13a10476e18e30c46
Pongstr <twiz.tickler@gmail.com>
2015-08-30 16:38:42 +0800

    fixed typo

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,7 +5,7 @@
 #   include sublime_text::config
 class sublime_text::config {
   $dir = "/Users/${::luser}/Library/Application Support/Sublime Text 3"
-  $pkgcontrol = "${dir}/Installed Pacakges"
+  $pkgcontrol = "${dir}/Installed Packages"
   $packagedir = "${dir}/Packages"
 
   file { [$dir, $pkgcontrol, $packagedir]:
